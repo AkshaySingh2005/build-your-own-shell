fc3694f6f10ffa87b1c084db6e3f5932dc0f45bd
AkshaySingh2005 <akshaysengar15671@google.com>
2025-12-24 21:31:59 +0530

    exit

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,14 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  while(true){
-    std::cout<<"$ ";
-    std::string input;
-    std::getline(std::cin,input);
+  
+  std::cout<<"$ ";
+  std::string input;
+  std::getline(std::cin,input);
+  do{
     std::cout<<input<<": command not found"<<std::endl;
   }
+  while(input == "exit");
 
   
 
