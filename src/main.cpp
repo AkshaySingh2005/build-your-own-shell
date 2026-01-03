5eaa16ff8ee5c5fa929e53baee081977f0ef518f
AkshaySingh2005 <akshaysengar15671@google.com>
2026-01-03 21:50:39 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -34,27 +34,43 @@ std::vector<std::string> split_path(const std::string& path) {
 std::vector<std::string> parse_args(const std::string& input) {
     std::vector<std::string> args;
     std::string current;
+
     bool in_single_quote = false;
     bool in_double_quote = false;
 
-    for (size_t i = 0; i < input.length(); ++i) {
+    for (size_t i = 0; i < input.size(); ++i) {
         char c = input[i];
 
+        
+        if (!in_single_quote && !in_double_quote && c == '\\') {
+            if (i + 1 < input.size()) {
+                current += input[i + 1]; 
+                i++;                     
+            }
+            continue;
+        }
+
+        
         if (c == '\'' && !in_double_quote) {
             in_single_quote = !in_single_quote;
+            continue; 
         }
-        else if (c == '"' && !in_single_quote) {
+
+        if (c == '"' && !in_single_quote) {
             in_double_quote = !in_double_quote;
+            continue; 
         }
-        else if (c == ' ' && !in_single_quote && !in_double_quote) {
+
+       
+        if (c == ' ' && !in_single_quote && !in_double_quote) {
             if (!current.empty()) {
                 args.push_back(current);
                 current.clear();
             }
+            continue;
         }
-        else {
-            current += c;
-        }
+
+        current += c;
     }
 
     if (!current.empty()) {
@@ -63,6 +79,7 @@ std::vector<std::string> parse_args(const std::string& input) {
 
     return args;
 }
+
 
 
 
